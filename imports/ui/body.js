d471496531e68c5c7a399ee395a392f29dcab1aa
meddlin <drushing.dev@gmail.com>
2016-07-25 21:47:29 -0500

    More from 'todos/tasks' tutorial. Can create and checkoff tasks.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,10 +1,44 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
+import './task.js';
 import './body.html';
 
 Template.body.helpers({
 	tasks() {
-		return Tasks.find({});
+		const instance = Template.instance();
+		if (instance.state.get('hideCompleted')) {
+			return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+		}
+
+		return Tasks.find({}, { sort: { createdAt: -1 } });
 	}
+});
+
+Template.body.events({
+	'submit .new-task'(event) {
+		event.preventDefault();
+
+		// Get value from form element
+		const target = event.target;
+		const text = target.text.value;
+
+		// Insert a task into the collection
+	    Tasks.insert({
+	      text,
+	      createdAt: new Date() // current time
+	    });
+
+	    // Clear form
+	    target.text.value = '';
+	},
+
+	'change .hide-completed input'(event, instance) {
+		instance.state.set('hideCompeleted', event.target.checked);
+	}
+});
+
+Template.boyd.onCreated(function bodyOnCreated() {
+	this.state = new ReactiveDict();
 });

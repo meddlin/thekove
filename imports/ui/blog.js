f8fbbcb2da28559391f94b3f072f4d3a77ea1e72
meddlin <drushing.dev@gmail.com>
2016-08-01 20:56:15 -0500

    rendering markdown on the 'blog' template

--- imports/ui/blog.js
+++ imports/ui/blog.js
@@ -1,1 +1,12 @@
 import './blog.html';
+
+Template.blog.helpers({
+	body() {
+		return "##Testing **123**";
+		/*return "meteor-markdown =============== /nGithub flavor Markdown parser for Meteor based on marked.js";*/
+
+		/*let converter = new Showdown.converter();
+		let markup = converter.makeHtml('Testing **123**');
+		return markup;*/
+	}
+});

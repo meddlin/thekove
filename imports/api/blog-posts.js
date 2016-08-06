1b996fa9de6c3ed06b9fefd06ba966b683afede7
meddlin <drushing.dev@gmail.com>
2016-08-06 02:54:33 -0500

    added packages aldeed:simple-schema, aldeed:collection2 and attempting a basic schema for BlogPosts.schema

--- imports/api/blog-posts.js
+++ imports/api/blog-posts.js
@@ -3,3 +3,31 @@ import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 
 export const BlogPosts = new Mongo.Collection('blog_posts');
+
+BlogPosts.schema = new SimpleSchema({
+	title: {
+		type: String
+	},
+	tags: {
+		type: [String]
+	},
+	slug: {
+		type: String
+	},
+	description: {
+		type: String
+	},
+	body: {
+		type: String
+	},
+	mode: {
+		type: String
+	},
+
+	updatedAt: {
+		type: Date
+	},
+	createdAt: {
+		type: Date
+	}
+});

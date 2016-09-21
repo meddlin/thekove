e0470568f383414a79d7c5d53c51cfa6a6ea524b
meddlin <drushing.dev@gmail.com>
2016-09-20 22:00:55 -0500

    blog posts have header styling and black border on left side on /blog

--- imports/ui/blog.js
+++ imports/ui/blog.js
@@ -7,13 +7,13 @@ import './blog.html';
 Template.blog.helpers({
 	docs() {
 		return BlogPosts.find();
-	},
+	}/*,
 	body() {
 		return "##Testing **123**";
-		/*return "meteor-markdown =============== /nGithub flavor Markdown parser for Meteor based on marked.js";*/
+	}*/
+	/*return "meteor-markdown =============== /nGithub flavor Markdown parser for Meteor based on marked.js";*/
 
 		/*let converter = new Showdown.converter();
 		let markup = converter.makeHtml('Testing **123**');
 		return markup;*/
-	}
 });

8e65e5543863daaf40ca6486e9273b96ba90b101
meddlin <drushing.dev@gmail.com>
2016-11-16 00:41:04 -0600

    configured default user to be assigned with Meteor.settings

--- server/main.js
+++ server/main.js
@@ -12,8 +12,10 @@ Meteor.startup(() => {
   // code to run on server at startup
   if (Meteor.users.find().count() == 0) {
   	let defaultUserId = Accounts.createUser({
-  		email: "admin@admin.com",
-  		password: "password"
+  		/*email: "admin@admin.com",
+  		password: "password"*/
+  		email: Meteor.settings.defaultUserEmail,
+  		password: Meteor.settings.defaultUserPassword
   	});
   	return Roles.addUsersToRoles(defaultUserId, 'admin');
   }

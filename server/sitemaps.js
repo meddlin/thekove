4c159f9586a439392726a5c39409eb4e2fecad20
meddlin <drushing.dev@gmail.com>
2016-12-09 23:31:32 -0600

    added '/about' to the dynamic sitemap

--- server/sitemaps.js
+++ server/sitemaps.js
@@ -3,6 +3,7 @@ import { BlogPosts } from '../imports/api/blog-posts.js';
 sitemaps.add('/sitemap.xml', function() {
 
 	var out = [];
+	out.push({ page: '/about' });
 	var pages = BlogPosts.find({mode: "public"}).fetch();
 	_.each(pages, function(p) {
 		out.push({

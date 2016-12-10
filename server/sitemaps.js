e6f41ce34fa8bc2dd57488deacab2f1791892cf4
meddlin <drushing.dev@gmail.com>
2016-12-09 23:27:50 -0600

    added package gadicohen:sitemaps. Created a dynamic sitemap configuration for the blog entires.

---
+++ server/sitemaps.js
@@ -0,0 +1,13 @@
+import { BlogPosts } from '../imports/api/blog-posts.js';
+
+sitemaps.add('/sitemap.xml', function() {
+
+	var out = [];
+	var pages = BlogPosts.find({mode: "public"}).fetch();
+	_.each(pages, function(p) {
+		out.push({
+			page: '/blog/' + p._id
+		});
+	});
+	return out;
+});

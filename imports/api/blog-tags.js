219e66ce3773a493db13754c8ddd8925231b292b
meddlin <drushing.dev@gmail.com>
2016-12-15 23:22:39 -0600

    BlogTags schema

--- imports/api/blog-tags.js
+++ imports/api/blog-tags.js
@@ -3,3 +3,16 @@ import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 
 export const BlogTags = new Mongo.Collection('blog_tags');
+
+BlogTags.schema = new SimpleSchema({
+	name: {
+		type: String
+	},
+
+	updatedAt: {
+		type: Date
+	},
+	createdAt: {
+		type: Date
+	}
+});

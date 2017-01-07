d529e0c25563feed848bbac76884e0bb7aaaf413
meddlin <drushing.dev@gmail.com>
2017-01-07 00:24:24 -0600

    fixed naive implementation of template level subscription on 'post' template

--- imports/ui/post.js
+++ imports/ui/post.js
@@ -6,26 +6,28 @@ import './post.html';
 
 Template.post.helpers({
 	doc() {
-		var sub = Template.instance().BlogPostsSub.get();
-		if (sub.ready()) {
-			let post = BlogPosts.findOne();
-			DocHead.setTitle('TheKove -- ' + post.title);
-			return post;
-		}
+		let post = Template.instance().myPosts();
+		DocHead.setTitle('TheKove -- ' + post.title);
+		return post;
 	}
 });
 
 Template.post.onCreated(function() {
-	this.BlogPostsSub = new ReactiveVar(null);
+	var instance = this;
 	var postId = FlowRouter.getParam("_id");
 
-	this.autorun(() => {
-		Template.instance().BlogPostsSub.set( Meteor.subscribe('BlogPosts_single', postId) );
+	instance.autorun(function() {
+		var subscription = instance.subscribe('BlogPosts_single', postId);
+		if (subscription.ready()) { // use instance.subscriptionsReady() for multiple subscriptions
+			console.log('BlogPosts_single is ready');
+		}
 	});
+
+	instance.myPosts = function() {
+		return BlogPosts.findOne();
+	};
 });
 
 Template.post.onDestroyed(function() {
-	var subToStop = Template.instance().BlogPostsSub.get();
-	subToStop.stop();
 	DocHead.setTitle('');
 });

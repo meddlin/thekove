8d35d56d84a63a2748729972afccf228edcb236e
meddlin <drushing.dev@gmail.com>
2017-01-24 23:04:26 -0600

    A working and SEO-friendly routing pattern is in place now. Updated 'post' template and links on 'landing' to use the new routes.

--- imports/startup/router.js
+++ imports/startup/router.js
@@ -42,31 +42,24 @@ FlowRouter.route('/blog', {
 		DocHead.setTitle('TheKove -- Blog');
 	}
 });
-/* a routing experiement */
-FlowRouter.route('/blog/cpat', {
+FlowRouter.route('/blog/tag/cpat', {
 	action: function(params) {
 		BlazeLayout.render('mainLayout', {content: 'cpat'});
 		DocHead.setTitle('TheKove -- CPAT');
 	}
 });
-FlowRouter.route('/blog/:_slug', {
+FlowRouter.route('/blog/tag/:_slug', {
 	action: function(params) {
 		BlazeLayout.render('mainLayout', {content: 'section'});
 		DocHead.setTitle('TheKove -- ' + params._slug);
 	}
 });
-FlowRouter.route('/blog/:_name/:_slug', {
+FlowRouter.route('/blog/:_postTitle', {
 	action: function(params) {
 		BlazeLayout.render('mainLayout', {content: 'post'});
+		DocHead.setTitle('TheKove -- ' + params._postTitle);
 	}
 });
-/****/
-/*FlowRouter.route('/blog/:_id', {   // already broken
-	action: function(params) {
-		BlazeLayout.render('mainLayout', {content: 'post'});
-	}
-});*/
-
 
 FlowRouter.route('/documents', {
 	action: function(params) {

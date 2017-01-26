e12759795e82059791993f619453596f8d3c15ad
meddlin <drushing.dev@gmail.com>
2017-01-26 00:04:31 -0600

    sitemap includes tag routes for sections, and entries based on BlogPosts is tied to 'slug' instead of '_id'

--- server/sitemaps.js
+++ server/sitemaps.js
@@ -1,15 +1,27 @@
 import { BlogPosts } from '../imports/api/blog-posts.js';
+import { BlogTags } from '../imports/api/blog-tags.js';
 
 sitemaps.add('/sitemap.xml', function() {
 
 	var out = [];
 	out.push({ page: '/about' });
 	out.push({ page: '/privacy' });
-	var pages = BlogPosts.find({mode: "public"}).fetch();
-	_.each(pages, function(p) {
+
+	var tags = BlogTags.find( {slug: {$exists: true}} ).fetch();
+	_.each(tags, function(t) {
 		out.push({
-			page: '/blog/' + p._id
+			page: '/blog/tag/' + t.slug
 		});
 	});
+
+	var pages = BlogPosts.find(
+		{mode: "public"},
+		{slug: {$exists: true}} ).fetch();
+	_.each(pages, function(p) {
+		out.push({
+			page: '/blog/' + p.slug
+		});
+	});
+
 	return out;
 });
